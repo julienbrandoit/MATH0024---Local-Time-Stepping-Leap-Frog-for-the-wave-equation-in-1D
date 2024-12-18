74d307c2f227ea49aaa33c0e7c865a89d24c6a32
Julien Brandoit <julienbrandoit@gmail.com>
2024-12-18 15:04:57 +0100

    working

--- hw2.py
+++ hw2.py
@@ -75,6 +75,23 @@ class Element1D:
         v[1, mask] = 1 / self.h * (x[mask] - self.nodes[0].x)
 
         return v if x.size > 1 else v[:, 0]
+    
+    def dphi(self, x):
+        """
+        TODO
+        """
+
+        x = np.atleast_1d(x)
+
+        # Create a mask to exclude points outside the element
+        mask = (x >= self.nodes[0].x) & (x <= self.nodes[1].x)
+
+        v = np.zeros((2, *x.shape))
+
+        v[0, mask] = 1 / self.h
+        v[1, mask] = -1 / self.h
+
+        return v if x.size > 1 else v[:, 0]
 
     def int_phi_phi(self, i, j):
         """
@@ -96,21 +113,14 @@ class Element1D:
 
     def int_dphi_dphi(self, i, j):
         """
-        Computes the integral of the derivatives of two shape functions.
-        Since we have linear shape functions, the derivative is a constant.
-        If i == j, the derivative is 1/h, otherwise it is -1/h.
-
-        Parameters:
-        - i, j : int, indices of the shape functions.
-
-        Returns:
-        - float, integral result.
+        TODO
         """
 
-        if i == j:
-            return 1 / self.h
-        else:
-            return -1 / self.h
+        def integrand(x):
+            dphi = self.dphi(x)
+            return dphi[i] * dphi[j]
+
+        return fixed_quad(integrand, self.nodes[0].x, self.nodes[1].x, n=1)[0]
 
     def mass_stiffness_matrices(self):
         """
@@ -453,13 +463,17 @@ class LocalTimeSteppingFEM1DLeapFrog():
 
 # == Plotting functions ==
 
-def compare_time_evolution(problemSolver_as_a_function_of_dt, T, t_eval, dt_list):
-    fig, ax = plt.subplots(len(t_eval), len(dt_list), figsize=(8, 30), sharex=True, sharey=True)
+def compare_time_evolution(problemSolver_as_a_function_of_dt, T, t_eval, dt_list, title):
+    """
+    TODO
+    """
+
+    fig, ax = plt.subplots(len(t_eval), len(dt_list), figsize=(16, 20), sharex=True, sharey=True)
     for i, dt in enumerate(dt_list):
         print(f"Computing solution for dt = {dt}...")
         problem = problemSolver_as_a_function_of_dt(dt)
         solution = problem.solve(T, t_eval)
-
+        print("... done. Plotting (can take some time) ...")
         for j, u in enumerate(solution):
             ax[j, i].plot([node.x for node in problem.fem.nodes], u, marker="o", markersize=5, linewidth=2)
             ax[j, i].set_xlim(0, problem.L)
@@ -472,7 +486,7 @@ def compare_time_evolution(problemSolver_as_a_function_of_dt, T, t_eval, dt_list
             if j == len(t_eval) - 1:
                 ax[j, i].set_xlabel(r"$x$", fontsize=20)
             if j == 0:
-                ax[j, i].set_title(r"$\Delta t = $" + f"{dt:.3f}", fontsize=20)
+                ax[j, i].set_title(r"$\Delta t = $" + f"{dt:.4f}", fontsize=20)
             # put the outeredge of the subplot in black
             for spine in ax[j, i].spines.values():
                 spine.set_edgecolor('black')
@@ -483,7 +497,7 @@ def compare_time_evolution(problemSolver_as_a_function_of_dt, T, t_eval, dt_list
 
     # Add a vertical time arrow on the right
     arrow_start_y = 0.875  # Start of arrow in figure coordinates
-    arrow_end_y = 0.05  # End of arrow in figure coordinates
+    arrow_end_y = 0.075  # End of arrow in figure coordinates
     arrow_x = 0.92  # X position of the arrow in figure coordinates
 
     # Add the arrow
@@ -517,13 +531,15 @@ def compare_time_evolution(problemSolver_as_a_function_of_dt, T, t_eval, dt_list
 
     # Add a time label at the top of the arrow
     fig.text(arrow_x + 0.01, arrow_end_y + 0.02, "Time", ha="left", fontsize=16)
+    # save the file
+    plt.savefig(title, bbox_inches='tight')
     plt.show()
 
 if __name__ == "__main__":
     c = -1.0
     L = 4.0
     sigma = 0.4
-    T = 90.0
+    T = 9.0
 
     def u0(x, sigma=1., L=1.):
         return 1./(np.sqrt(2*np.pi)*sigma) * np.exp(-(x - L/2)**2/(2*sigma**2))
@@ -531,93 +547,100 @@ if __name__ == "__main__":
     def v0(x):
         return c
     
-    if False:
-        # == We define both the manual and classical FEM models ==
-        # We generate a figure to show the mesh and the initial condition
+    # == We define both the manual and classical FEM models ==
+    # We generate a figure to show the mesh and the initial condition
 
-        print("\n == Part 1 ==\n")
+    print("\n == Part 1 ==\n")
 
-        h_coarse = 0.1
-        n_elements_coarse = int(L/h_coarse)
-        p = 4
-        n_elements_refined = 2
+    h_coarse = 0.1
+    n_elements_coarse = int(L/h_coarse)
+    p = 4
+    n_elements_refined = 2
 
-        refined_mesh = []
+    refined_mesh = []
 
-        for i, x in enumerate(np.arange(0, L+h_coarse, h_coarse)):
-            refined_mesh.append(x)
-            if 1 <= x < 1 + h_coarse * n_elements_refined:
-                for j in range(1, p):
-                    refined_mesh.append(x + j * h_coarse / p)
+    for i, x in enumerate(np.arange(0, L+h_coarse, h_coarse)):
+        refined_mesh.append(x)
+        if 1 <= x < 1 + h_coarse * n_elements_refined:
+            for j in range(1, p):
+                refined_mesh.append(x + j * h_coarse / p)
 
-        regular_mesh_problem = ClassicalFEM1DLeapFrog(n_elements_or_mesh=n_elements_coarse, dt=0.01, c=c, L=L, u0=lambda x: u0(x, sigma, L), v0=v0)
-        refined_mesh_problem = ClassicalFEM1DLeapFrog(n_elements_or_mesh=refined_mesh, dt=0.01, c=c, L=L, u0=lambda x: u0(x, sigma, L), v0=v0)
+    regular_mesh_problem = ClassicalFEM1DLeapFrog(n_elements_or_mesh=n_elements_coarse, dt=0.1, c=c, L=L, u0=lambda x: u0(x, sigma, L), v0=v0)
+    refined_mesh_problem = ClassicalFEM1DLeapFrog(n_elements_or_mesh=refined_mesh, dt=0.1, c=c, L=L, u0=lambda x: u0(x, sigma, L), v0=v0)
 
-        # plot of the initial condition (no title) and the computed u_previous
-        fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+    # plot of the initial condition (no title) and the computed u_previous
+    fig, ax = plt.subplots(1, 2, figsize=(16, 5))
 
-        y_mesh = 0
-        for ax_i, problem in zip(ax, [regular_mesh_problem, refined_mesh_problem]):
-            ax_i.hlines(y_mesh, 0, L, colors='black', linestyles='solid')
-            for node in problem.fem.nodes:
-                ax_i.plot(node.x, y_mesh, 'k', marker='d', markersize=5)
-                ax_i.vlines(node.x, y_mesh, problem.u[node.global_idx], colors='black', linestyles='dashed', alpha=0.5, linewidth=1.5)
+    y_mesh = 0
+    for ax_i, problem in zip(ax, [regular_mesh_problem, refined_mesh_problem]):
+        ax_i.hlines(y_mesh, 0, L, colors='black', linestyles='solid')
+        for node in problem.fem.nodes:
+            ax_i.plot(node.x, y_mesh, 'k', marker='d', markersize=5, label="Node position" if node.global_idx == 0 else None)
+            ax_i.vlines(node.x, y_mesh, max(problem.u[node.global_idx], problem.u_previous[node.global_idx]), colors='black', linestyles='dashed', alpha=0.5, linewidth=1.5)
 
-            ax_i.set_xlim(0, L)
-            ax_i.set_ylim(-0.1, 1.2)
-            ax_i.xaxis.set_tick_params(labelsize=15)
-            ax_i.yaxis.set_tick_params(labelsize=15)
+        ax_i.set_xlim(0, L)
+        ax_i.set_ylim(-0.1, 1.2)
+        ax_i.xaxis.set_tick_params(labelsize=15)
+        ax_i.yaxis.set_tick_params(labelsize=15)
+        ax_i.set_xlabel(r"$x$", fontsize=20)
 
-        ax[0].plot([node.x for node in regular_mesh_problem.fem.nodes], regular_mesh_problem.u, marker="o", markersize=5, linewidth=2)
-        ax[1].plot([node.x for node in refined_mesh_problem.fem.nodes], refined_mesh_problem.u, marker="o", markersize=5, linewidth=2)
+    ax[0].plot([node.x for node in regular_mesh_problem.fem.nodes], regular_mesh_problem.u, marker="o", markersize=5, linewidth=2, label=r"$\mathbf{u}_0$")
+    ax[0].plot([node.x for node in regular_mesh_problem.fem.nodes], regular_mesh_problem.u_previous, marker="o", markersize=5, linewidth=2, label=r"$\mathbf{u}_{-1}$")
 
-        plt.tight_layout()
-        plt.show()
+    ax[1].plot([node.x for node in refined_mesh_problem.fem.nodes], refined_mesh_problem.u, marker="o", markersize=5, linewidth=2, label=r"$\mathbf{u}_0$")
+    ax[1].plot([node.x for node in refined_mesh_problem.fem.nodes], refined_mesh_problem.u_previous, marker="o", markersize=5, linewidth=2, label=r"$\mathbf{u}_{-1}$")
 
-        fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+    handles, labels = ax[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', fontsize=20, ncol=3)
 
-        # Find the min and max values for the color scale
-        vmin = min(problem.A.min() for problem in [regular_mesh_problem, refined_mesh_problem])
-        vmax = max(problem.A.max() for problem in [regular_mesh_problem, refined_mesh_problem])
+    plt.tight_layout(rect=[0, 0.1, 1, 1])  # Adjust layout to make space for the legend
+    # save the figure as initial_approximation.pdf
+    plt.savefig("initial_approximation.pdf", bbox_inches='tight')
+    plt.show()
 
-        for ax, problem in zip(axs, [regular_mesh_problem, refined_mesh_problem]):
-            im = ax.imshow(problem.A.toarray(), cmap=sns_color, vmin=vmin, vmax=vmax)
-            ax.set_title(f"Mesh with {len(problem.fem.nodes)} nodes", fontsize=15)
-            ax.set_xticks([])
-            ax.set_yticks([])
-            ax.set_xlabel("Node index", fontsize=15)
-            ax.set_ylabel("Node index", fontsize=15)
+    # Plot of both meshes (regular is red and refined is blue) : 
+    fig, ax = plt.subplots(figsize=(10, 3))
 
-        # Add a common colorbar
-        cbar = fig.colorbar(im, ax=axs, orientation='vertical', fraction=0.02, pad=0.04)
-        cbar.set_label('Matrix value', fontsize=15)
-        plt.show()
+    ax.plot([n.x for n in regular_mesh_problem.fem.nodes], np.zeros(len(regular_mesh_problem.fem.nodes))+1, 'ro', markersize=10, alpha=0.5, label="Regular mesh")
+    ax.plot([n.x for n in refined_mesh_problem.fem.nodes], np.zeros(len(refined_mesh_problem.fem.nodes)), 'bo', markersize=10, alpha=0.5, label="Refined mesh")
 
-        # == Question 2 : numerical exploration of the stability criterion on the coarse mesh ==
-        # We compute the solution for different values of dt
-        # The theoretical stability criterion is dt <= h_coarse / abs(c)
-        # In our case, c = -1, so the stability criterion is dt <= h_coarse
-        print("\n == Part 2 ==\n")
+    ax.set_xlim(0, L)
+    ax.set_ylim(-0.1, 1.1)
+    ax.xaxis.set_tick_params(labelsize=15)
+    ax.set_yticks([])
+    ax.set_xlabel(r"$x$", fontsize=20)
 
-        dt_eval = 0.5
-        t_eval = np.arange(0, T + dt_eval, dt_eval)
-        dt_list = [h_coarse / 1.05, h_coarse, 1.05 * h_coarse]
-        regular_problemSolver_as_a_function_of_dt = lambda dt: ClassicalFEM1DLeapFrog(n_elements_or_mesh=n_elements_coarse, dt=dt, c=c, L=L, u0=lambda x: u0(x, sigma, L), v0=v0)
-        compare_time_evolution(regular_problemSolver_as_a_function_of_dt, T, t_eval, dt_list)
+    ax.legend(fontsize=15, loc='upper center', bbox_to_anchor=(0.5, -0.5), ncol=2)
+    plt.subplots_adjust(bottom=0.5)
+    # save as mesh_pts1.pdf
+    plt.savefig("mesh_pts1.pdf", bbox_inches='tight')
+    plt.show()
 
-        # == Question 3 : numerical exploration of the stability criterion on the refined mesh ==
-        # We compute the solution for different values of dt
-        # The theoretical stability criterion is dt <= h_fine / abs(c)
-        # In our case, c = -1, so the stability criterion is dt <= h_fine
-        print("\n == Part 3 ==\n")
+    # == Question 2 : numerical exploration of the stability criterion on the coarse mesh ==
+    # We compute the solution for different values of dt
+    # The theoretical stability criterion is dt <= h_coarse / abs(c)
+    # In our case, c = -1, so the stability criterion is dt <= h_coarse
+    print("\n == Part 2 ==\n")
 
-        h_fine = h_coarse / p
-        dt_list = [h_fine / 1.05, h_fine, 1.05 * h_fine]
-        refined_problemSolver_as_a_function_of_dt = lambda dt: ClassicalFEM1DLeapFrog(n_elements_or_mesh=refined_mesh, dt=dt, c=c, L=L, u0=lambda x: u0(x, sigma, L), v0=v0)
-        compare_time_evolution(refined_problemSolver_as_a_function_of_dt, T, t_eval, dt_list)
+    dt_eval = 0.5
+    t_eval = np.arange(0, T + dt_eval, dt_eval)
+    dt_list = [h_coarse * 0.95, h_coarse, 1.05 * h_coarse]
+    regular_problemSolver_as_a_function_of_dt = lambda dt: ClassicalFEM1DLeapFrog(n_elements_or_mesh=n_elements_coarse, dt=dt, c=c, L=L, u0=lambda x: u0(x, sigma, L), v0=v0)
+    compare_time_evolution(regular_problemSolver_as_a_function_of_dt, T, t_eval, dt_list, title="regular_mesh_sol.pdf")
 
-        # == Question 4 : Local time stepping ==
-        # We implement the local time stepping method
+    # == Question 3 : numerical exploration of the stability criterion on the refined mesh ==
+    # We compute the solution for different values of dt
+    # The theoretical stability criterion is dt <= h_fine / abs(c)
+    # In our case, c = -1, so the stability criterion is dt <= h_fine
+    print("\n == Part 3 ==\n")
+
+    h_fine = h_coarse / p
+    dt_list = [h_fine * 0.95, h_fine, 1.05 * h_fine]
+    refined_problemSolver_as_a_function_of_dt = lambda dt: ClassicalFEM1DLeapFrog(n_elements_or_mesh=refined_mesh, dt=dt, c=c, L=L, u0=lambda x: u0(x, sigma, L), v0=v0)
+    compare_time_evolution(refined_problemSolver_as_a_function_of_dt, T, t_eval, dt_list, title="refined_mesh_sol.pdf")
+
+    # == Question 4 : Local time stepping ==
+    # We implement the local time stepping method
 
     print("\n == Part 4 ==\n")
 
@@ -625,8 +648,8 @@ if __name__ == "__main__":
 
     h_coarse = 0.1
     n_elements_coarse = int(L/h_coarse)
-    p = 10
-    n_elements_refined = 10
+    p = 5
+    n_elements_refined = 2
 
     refined_mesh = []
     mask_is_fine_node = []
@@ -687,47 +710,10 @@ if __name__ == "__main__":
     plt.legend(["$u$", "$u_{previous}$", "$u_{problem2}$", "$u_{previous, problem2}$"], fontsize=15)
     plt.show()
 
-    # plot all the matrices
-    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
-
-    im = axs[0, 0].imshow(problem.A_cc.toarray(), cmap=sns_color)
-    axs[0, 0].set_title("A_cc", fontsize=15)
-    axs[0, 0].set_xticks([])
-    axs[0, 0].set_yticks([])
-    axs[0, 0].set_xlabel("Coarse node index", fontsize=15)
-    axs[0, 0].set_ylabel("Coarse node index", fontsize=15)
-
-    im = axs[0, 1].imshow(problem.A_cf.toarray(), cmap=sns_color)
-    axs[0, 1].set_title("A_cf", fontsize=15)
-    axs[0, 1].set_xticks([])
-    axs[0, 1].set_yticks([])
-    axs[0, 1].set_xlabel("Fine node index", fontsize=15)
-    axs[0, 1].set_ylabel("Coarse node index", fontsize=15)
-
-    im = axs[1, 0].imshow(problem.A_fc.toarray(), cmap=sns_color)
-    axs[1, 0].set_title("A_fc", fontsize=15)
-    axs[1, 0].set_xticks([])
-    axs[1, 0].set_yticks([])
-    axs[1, 0].set_xlabel("Coarse node index", fontsize=15)
-    axs[1, 0].set_ylabel("Fine node index", fontsize=15)
-
-    im = axs[1, 1].imshow(problem.A_ff.toarray(), cmap=sns_color)
-    axs[1, 1].set_title("A_ff", fontsize=15)
-    axs[1, 1].set_xticks([])
-    axs[1, 1].set_yticks([])
-    axs[1, 1].set_xlabel("Fine node index", fontsize=15)
-    axs[1, 1].set_ylabel("Fine node index", fontsize=15)
-
-    # Add a common colorbar
-    cbar = fig.colorbar(im, ax=axs, orientation='vertical', fraction=0.02, pad=0.04)
-    cbar.set_label('Matrix value', fontsize=15)
-    plt.show()
-
     # We compute the solution
     solution = problem.solve(T, t_eval = np.arange(0, T, 0.1))
     solution2 = problem2.solve(T, t_eval = np.arange(0, T, 0.1))
 
-    plt.show()
     # make an animation with the difference between both solutions
     fig, ax = plt.subplots(2, 1, figsize=(10, 10))
 
